0870402c54d05da32e3ba889e5d632e7e64205c8
TheSpyGeek <mathieurire@gmail.com>
2017-02-14 16:40:36 +0100

    ajout de la fonction matrice

--- Algo6/TD2_4/sousseq.java
+++ Algo6/TD2_4/sousseq.java
@@ -10,12 +10,16 @@ class sousseq {
 
 	static public void main(String [] args){
 
-		S1 = "AABAAACCABBA";
-		S2 = "ABBBAACDABCDA";
+		 S1 = "AABAAACCABBA";
+		 S2 = "ABBBAACDABCDA";
+
+		// S1 = "ABBC";
+		// S2 = "ABBAC";
 
 		//System.out.println(S2.charAt(S2.length()-1));
-
-		System.out.println("Plus grande sequence commune "+plssc(0,0));
+		System.out.println("Longeur plus grande "+matrice());
+		// matrice();
+		//System.out.println("Plus grande sequence commune "+plssc(0,0));
 	} 
 
 	static public int max(int a, int b){
@@ -39,6 +43,43 @@ class sousseq {
 			return -1;
 		}
 
+	}
+
+	static public int matrice(){
+
+		int i = 0, j=0;
+
+		int imax = S1.length()+1;
+		int jmax = S2.length()+1;
+
+		int mat[][] = new int[imax][jmax];
+
+		for(i=0;i<imax;i++){
+			for(j=0;j<jmax; j++){
+				mat[i][j] = 0;
+			}
+		}
+
+		for(i=1;i<imax;i++){
+			for(j=1;j<jmax; j++){
+				if(S1.charAt(i-1) == S2.charAt(j-1)){
+					mat[i][j] = mat[i-1][j-1] +1;
+				} else {
+					mat[i][j] = max(mat[i][j-1], mat[i-1][j]);
+				}
+
+			}
+		}
+
+		for(i=0;i<imax;i++){
+			for(j=0;j<jmax; j++){
+				System.out.print(mat[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+
+		return mat[i-1][j-1];
 	}
 
 
